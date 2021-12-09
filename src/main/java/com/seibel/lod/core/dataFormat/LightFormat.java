fe1b2c268346b44a4204db6fb2171aa7b3aa51e4
Morippi <Morippi>
2021-12-09 12:57:35 +0100

    Added new format classes

--- src/main/java/com/seibel/lod/core/dataFormat/LightFormat.java
+++ src/main/java/com/seibel/lod/core/dataFormat/LightFormat.java
@@ -2,4 +2,9 @@ package com.seibel.lod.core.dataFormat;
 
 public class LightFormat
 {
+	public final static int BLOCK_LIGHT_SHIFT = 4;
+	public final static int SKY_LIGHT_SHIFT = 0;
+	
+	public final static long BLOCK_LIGHT_MASK = 0b1111;
+	public final static long SKY_LIGHT_MASK = 0b1111;
 }
