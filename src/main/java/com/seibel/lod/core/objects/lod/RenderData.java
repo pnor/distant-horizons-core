4153d77838ea97682a82943a3784c78c41304cad
Morippi <Morippi>
2021-12-20 21:56:30 +0100

    added code to RenderData

--- src/main/java/com/seibel/lod/core/objects/lod/RenderData.java
+++ src/main/java/com/seibel/lod/core/objects/lod/RenderData.java
@@ -1,44 +1,46 @@
 package com.seibel.lod.core.objects.lod;
 
+import com.seibel.lod.core.util.LodUtil;
+
 public class RenderData implements RenderDataContainer
 {
 	public final byte detailLevel;
 	public final int size;
 	
-	public byte[] hasChildToRendered;
-	public byte[] hasRenderedChild;
+	public boolean[] hasChildToRendered;
+	public boolean[] hasRenderedChild;
 	public boolean[] toBeRendered;
 	public boolean[] rendered;
 	
 	RenderData(byte detailLevel)
 	{
 		this.detailLevel = detailLevel;
-		this.size =
+		this.size = 1 << (LodUtil.REGION_DETAIL_LEVEL - detailLevel);
 	}
 	
 	public void setChildToRendered(int posX, int posZ, boolean newValue){
-		hasChildToRendered[][]
+		hasChildToRendered[posX*size + posZ] = newValue;
 	}
 	public void setRenderedChild(int posX, int posZ, boolean newValue){
-	
+		hasRenderedChild[posX*size + posZ] = newValue;
 	}
 	public void setToBeRendered(int posX, int posZ, boolean newValue){
-	
+		toBeRendered[posX*size + posZ] = newValue;
 	}
 	public void setRendered(int posX, int posZ, boolean newValue){
-	
+		rendered[posX*size + posZ] = newValue;
 	}
 	
 	public boolean isChildToRendered(int posX, int posZ){
-	
+		return hasChildToRendered[posX*size + posZ];
 	}
 	public boolean isChildRendered(int posX, int posZ){
-	
+		return hasRenderedChild[posX*size + posZ];
 	}
 	public boolean isToBeRendered(int posX, int posZ){
-	
+		return toBeRendered[posX*size + posZ];
 	}
 	public boolean isRendered(int posX, int posZ){
-	
+		return rendered[posX*size + posZ];
 	}
 }
