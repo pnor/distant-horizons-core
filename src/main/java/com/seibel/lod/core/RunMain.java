a14046ef8803ccb3f623aeaec25accf3c3f1f776
coolGi2007 <sasanaps@hotmail.com>
2022-02-20 11:43:41 +0000

    Added something so you can run the jar(dosnt do anything yet)

---
+++ src/main/java/com/seibel/lod/core/RunMain.java
@@ -0,0 +1,7 @@
+package com.seibel.lod.core;
+
+public class RunMain {
+    public static void main(String[] args){
+        System.out.println("Why are you running the jar, this isn't done yet");
+    }
+}
