0edca2402d8ca75dd0dcbe2923a5d2133d3f48ae
coolGi2007 <sasanaps@hotmail.com>
2022-02-20 22:23:03 +0000

    Renamed RunMain to JarMain

---
+++ src/main/java/com/seibel/lod/core/JarMain.java
@@ -0,0 +1,7 @@
+package com.seibel.lod.core;
+
+public class JarMain {
+    public static void main(String[] args){
+        System.out.println("Why are you running the jar, this isn't done yet  >:(");
+    }
+}
--- src/main/java/com/seibel/lod/core/RunMain.java
+++
@@ -1,7 +0,0 @@
-package com.seibel.lod.core;
-
-public class RunMain {
-    public static void main(String[] args){
-        System.out.println("Why are you running the jar, this isn't done yet");
-    }
-}
