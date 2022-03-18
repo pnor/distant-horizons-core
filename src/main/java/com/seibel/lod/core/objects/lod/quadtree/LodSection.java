0b96ca8509e27a8b0827e10cd7565d91d69459c6
Morippi <Morippi>
2022-03-18 16:04:31 +0100

    changed names of variables

--- src/main/java/com/seibel/lod/core/objects/lod/quadtree/LodSection.java
+++ src/main/java/com/seibel/lod/core/objects/lod/quadtree/LodSection.java
@@ -9,8 +9,8 @@ public class LodSection
 	public final int levelPosZ;
 	public final int horizontalSize;
 	public final int verticalSize;
-	public final int numberOfBlockBiomePerLod;
-	public final byte sectionGenerationMode;
+	public final int idPerLod;
+	public final byte generationMode;
 	public boolean avoidPregeneratedChunk;
 	
 	//Position data hold information about each level position like generation mode used, number of vertical lods in that area...
@@ -26,19 +26,19 @@ public class LodSection
 	//We could just reference
 	private int[] BlockBiomeId;
 	
-	//BlockBiomeFrequency for each
+	//BlockBiomeFrequency for each lod BlockBiomeId
 	private byte[] BlockBiomeFrequency;
 	
-	public LodSection(int detail, int horizontalSize, int verticalSize, int levelPosX, int levelPosZ, byte sectionGenerationMode, boolean avoidPregeneratedChunk, int numberOfBlockBiomePerLod)
+	public LodSection(int detail, int horizontalSize, int verticalSize, int levelPosX, int levelPosZ, byte generationMode, boolean avoidPregeneratedChunk, int idPerLod)
 	{
 		this.detail = detail;
 		this.levelPosX = levelPosX;
 		this.levelPosZ = levelPosZ;
 		this.horizontalSize = horizontalSize;
 		this.verticalSize = verticalSize;
-		this.sectionGenerationMode = sectionGenerationMode;
+		this.generationMode = generationMode;
 		this.avoidPregeneratedChunk = avoidPregeneratedChunk;
-		this.numberOfBlockBiomePerLod = numberOfBlockBiomePerLod;
+		this.idPerLod = idPerLod;
 		//Now we should search if there is a file with this values
 		//if so we load it and end the process
 		
@@ -46,7 +46,7 @@ public class LodSection
 		positionData = new byte[horizontalSize*horizontalSize];
 		verticalData = new int[horizontalSize*horizontalSize*verticalSize];
 		lightsData = new byte[horizontalSize*horizontalSize*verticalSize];
-		BlockBiomeId = new int[horizontalSize*horizontalSize*verticalSize*numberOfBlockBiomePerLod];
-		BlockBiomeFrequency = new byte[horizontalSize*horizontalSize*verticalSize*numberOfBlockBiomePerLod];
+		BlockBiomeId = new int[horizontalSize*horizontalSize*verticalSize*idPerLod];
+		BlockBiomeFrequency = new byte[horizontalSize*horizontalSize*verticalSize*idPerLod];
 	}
 }
