22bc6b5c9a3e0e0f478cc882380229d4a912fee4
James Seibel <jeseibel@gondtc.com>
2022-04-06 22:21:41 -0500

    Add the copyright header to files missing it

--- src/main/java/com/seibel/lod/core/enums/config/DropoffQuality.java
+++ src/main/java/com/seibel/lod/core/enums/config/DropoffQuality.java
@@ -2,7 +2,8 @@
  *    This file is part of the Distant Horizon mod (formerly the LOD Mod),
  *    licensed under the GNU GPL v3 License.
  *
- *    Copyright (C) 2022 Tom Lee (TomTheFurry)
+ *    Copyright (C) 2022  Tom Lee (TomTheFurry)
+ *    Copyright (C) 2020  James Seibel
  *
  *    This program is free software: you can redistribute it and/or modify
  *    it under the terms of the GNU General Public License as published by
@@ -16,7 +17,7 @@
  *    You should have received a copy of the GNU General Public License
  *    along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
-
+ 
 package com.seibel.lod.core.enums.config;
 
 /**
