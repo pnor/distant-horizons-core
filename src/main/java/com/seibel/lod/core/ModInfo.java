ce7217bad564c642c93a05dd4c2f97cde0769cba
James Seibel <jeseibel@gondtc.com>
2022-04-11 07:15:51 -0500

    update the version number in ModInfo

--- src/main/java/com/seibel/lod/core/ModInfo.java
+++ src/main/java/com/seibel/lod/core/ModInfo.java
@@ -41,5 +41,5 @@ public final class ModInfo
 	/** Human readable version of NAME */
 	public static final String READABLE_NAME = "Distant Horizons";
 	public static final String API = "LodAPI";
-	public static final String VERSION = "1.6.2a";
+	public static final String VERSION = "1.6.3a-dev";
 }
