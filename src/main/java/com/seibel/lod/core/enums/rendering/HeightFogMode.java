175c48a49e563ae6f2847600c948f699af02e133
James Seibel <jeseibel@gondtc.com>
2022-04-13 21:57:48 -0500

    auto-format

--- src/main/java/com/seibel/lod/core/enums/rendering/HeightFogMode.java
+++ src/main/java/com/seibel/lod/core/enums/rendering/HeightFogMode.java
@@ -16,24 +16,26 @@
  *    You should have received a copy of the GNU General Public License
  *    along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
- 
+
 package com.seibel.lod.core.enums.rendering;
 
-public enum HeightFogMode {
-    ABOVE_CAMERA(true, true, false),
-    BELOW_CAMERA(true, false, true),
-    ABOVE_AND_BELOW_CAMERA(true, true, true),
-    ABOVE_SET_HEIGHT(false, true, false),
-    BELOW_SET_HEIGHT(false, false, true),
-    ABOVE_AND_BELOW_SET_HEIGHT(false, true, true);
-
-    public final boolean basedOnCamera;
-    public final boolean above;
-    public final boolean below;
-
-    HeightFogMode(boolean basedOnCamera, boolean above, boolean below) {
-        this.basedOnCamera = basedOnCamera;
-        this.above = above;
-        this.below = below;
-    }
+public enum HeightFogMode
+{
+	ABOVE_CAMERA(true, true, false),
+	BELOW_CAMERA(true, false, true),
+	ABOVE_AND_BELOW_CAMERA(true, true, true),
+	ABOVE_SET_HEIGHT(false, true, false),
+	BELOW_SET_HEIGHT(false, false, true),
+	ABOVE_AND_BELOW_SET_HEIGHT(false, true, true);
+	
+	public final boolean basedOnCamera;
+	public final boolean above;
+	public final boolean below;
+	
+	HeightFogMode(boolean basedOnCamera, boolean above, boolean below)
+	{
+		this.basedOnCamera = basedOnCamera;
+		this.above = above;
+		this.below = below;
+	}
 }
