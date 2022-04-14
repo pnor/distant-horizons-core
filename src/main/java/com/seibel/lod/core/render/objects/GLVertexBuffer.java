73f9edc091a545da4097c98cd98267a0be219b33
TomTheFurry <tomlee92502@yahoo.com>
2022-04-14 18:17:39 +0800

    Fix GpuUploadMethod that use mapping to use DynamicDraw instead of StaticDraw

--- src/main/java/com/seibel/lod/core/render/objects/GLVertexBuffer.java
+++ src/main/java/com/seibel/lod/core/render/objects/GLVertexBuffer.java
@@ -71,7 +71,8 @@ public class GLVertexBuffer extends GLBuffer
 	public ByteBuffer mapBuffer(int targetSize, GpuUploadMethod uploadMethod, int maxExpensionSize)
 	{
 		return super.mapBuffer(targetSize, uploadMethod, maxExpensionSize,
-				uploadMethod.useBufferStorage ? GL32.GL_MAP_WRITE_BIT : GL32.GL_STATIC_DRAW,
+				uploadMethod.useBufferStorage ? GL32.GL_MAP_WRITE_BIT :
+						uploadMethod.useEarlyMapping ? GL32.GL_DYNAMIC_DRAW : GL32.GL_STATIC_DRAW,
 				GL32.GL_MAP_WRITE_BIT | GL32.GL_MAP_UNSYNCHRONIZED_BIT | GL32.GL_MAP_INVALIDATE_BUFFER_BIT);
 	}
 }
