18097216657e9686cc057fab6247b51ef78776a1
coolGi2007 <sasanaps@hotmail.com>
2022-05-26 09:25:06 +0000

    Why dosnt F3Screen not have a .java at the end of its name?

--- src/main/java/com/seibel/lod/core/render/F3Screen.java
+++ src/main/java/com/seibel/lod/core/render/F3Screen.java
@@ -8,8 +8,7 @@ import java.util.List;
 public class F3Screen {
     public static List<String> f3List = Arrays.asList(
             "",
-            ModInfo.READABLE_NAME + " version: " + ModInfo.VERSION,
-            ""
+            ModInfo.READABLE_NAME + " version: " + ModInfo.VERSION
     );
     public static boolean renderCustomF3 = false;
 }
